39e63fd8dc21b10e180831eec63651271bf6b8cf
zhackzey <32985806+zhackzey@users.noreply.github.com>
2018-05-12 17:06:32 +0800

    add register page

--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,63 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+var rootDocment = 'https://162.105.175.115';
+function req(url, data, cb) {
+  wx.request({
+    url: rootDocment + url,
+    data: data,
+    method: 'post',
+    header: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    success: function (res) {
+      return typeof cb == "function" && cb(res.data)
+    },
+    fail: function () {
+      return typeof cb == "function" && cb(false)
+    }
+  })
+}
+
+function getReq(url, data, cb) {
+  wx.request({
+    url: rootDocment + url,
+    data: data,
+    method: 'get',
+    header: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    success: function (res) {
+      return typeof cb == "function" && cb(res.data)
+    },
+    fail: function () {
+      return typeof cb == "function" && cb(false)
+    }
+  })
+}
+
+// 去前后空格  
+function trim(str) {
+  return str.replace(/(^\s*)|(\s*$)/g, "");
+}
+
+// 提示错误信息  
+function isError(msg, that) {
+  that.setData({
+    showTopTips: true,
+    errorMsg: msg
+  })
+}
+
+// 清空错误信息  
+function clearError(that) {
+  that.setData({
+    showTopTips: false,
+    errorMsg: ""
+  })
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  req: req,
+  trim: trim,
+  isError: isError,
+  clearError: clearError,
+  getReq: getReq
 }
