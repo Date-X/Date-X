dec24b6666e2ac0caf6618c806e4301baee44733
burning846 <893070935@qq.com>
2018-05-17 21:13:05 +0800

    section jump
    点击分区模块可进入房间列表

--- pages/room/room.js
+++ pages/room/room.js
@@ -1,91 +1,75 @@
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    sectionmenu:[],
+    imgUrls: [
+      '../../images/roomlist.png',
+      '../../images/roomlist.png',
+      '../../images/roomlist.png',
+      '../../images/roomlist.png',
+      '../../images/roomlist.png',
+      '../../images/roomlist.png',
+      '../../images/roomlist.png'
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.fetchData();
+  
   },
-  fetchData: function () {
-    this.setData({
-      sectionmenu: [
-        {
-          'icon': './../../images/王者荣耀.jpg',
-          'text': '王者荣耀',
-          'url': '../rooms/rooms'
-        },
-        {
-          'icon': './../../images/吃鸡.jpg',
-          'text': '吃鸡',
-          'url': '../rooms/rooms'
-        },
-        {
-          'icon': './../../images/英雄联盟.jpg',
-          'text': '英雄联盟',
-          'url': '../rooms/rooms'
-        },
-        {
-          'icon': './../../images/狼人杀.jpg',
-          'text': '狼人杀',
-          'url': '../rooms/rooms'
-        }
-      ]
-    })
-  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
-  }
+  
+  },
+
+  
 })
