bc26498474891c485de36ae484e01bd3a234506e
zhackzey <32985806+zhackzey@users.noreply.github.com>
2018-05-29 19:52:08 +0800

    add homepage(index)

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,64 +1,71 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    /*wx.navigateTo({
-      url: '../logs/logs'
-    })*/
-    wx.scanCode({
-      onlyFromCamera : true,
-      type : 'qrCode',
-      success: (res) => {
-        console.log(res);
-        wx.request({
-          url: '',
-        })
-      }
-    })
+    indexmenu:[],
+    imgUrls:[]
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+    //生命周期函数--监听页面加载
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      indexmenu:[
+        {
+          'icon':'./../../images/KoG.jpg',
+          'text':'王者荣耀',
+          'url': '../roomlist/roomlist?id=1'
+        },
+        {
+          'icon': './../../images/LOL.jpg',
+          'text': '英雄联盟',
+          'url': '../roomlist/roomlist?id=3'
+        },
+        {
+          'icon': './../../images/PUBG.jpg',
+          'text': '绝地求生',
+          'url': '../roomlist/roomlist?id=2'
+        },
+        {
+          'icon': './../../images/wolf.jpg',
+          'text': '狼人杀',
+          'url': '../roomlist/roomlist?id=4'
+        }
+      ],
+      imgUrls:[
+        '../../images/swiper_pic1.png',
+        '../../images/swiper_pic1.png',
+        '../../images/swiper_pic1.png',
+        '../../images/swiper_pic1.png'
+      ]
     })
+  },
+    changeRoute:function(url){
+    wx.navigateTo({
+      url: `../${url}/${url}`
+    })
+  },
+  onReady:function(){
+    //生命周期函数--监听页面初次渲染完成
+    // console.log('onReady');
+  },
+  onShow :function(){
+    //生命周期函数--监听页面显示
+    // console.log('onShow');
+  },
+  onHide :function(){
+    //生命周期函数--监听页面隐藏
+    // console.log('onHide');
+  },
+  onUnload :function(){
+    //生命周期函数--监听页面卸载
+    // console.log('onUnload');
+  },
+  onPullDownRefresh:function(){
+    //页面相关事件处理函数--监听用户下拉动作
+    // console.log('onPullDownRefresh');
+  },
+  onReachBottom:function(){
+    //页面上拉触底事件的处理函数
+    // console.log('onReachBottom');
   }
 })
