6dd0087006c0aae534c89cbe40c99c98c9b1d15e
czy <czy@czydeMacBook-Pro.local>
2018-06-13 21:10:18 +0800

    add feature: delete room

--- pages/room/room.js
+++ pages/room/room.js
@@ -2,7 +2,6 @@ var timer;
 const app = getApp();
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -174,24 +173,42 @@ Page({
 
   delete_room: function () {
     var that = this
-    wx.request({
-      url: app.globalData.serverurl+'/usr/join',
-      data: {
-        room_id: parseInt(that.data.room_id),
-        open_id: app.globalData.openid,
-      },
-      dataType: 'json',
-      method: 'POST',
+    wx.showModal({
+      title: '确认删除？',
+      content: '您确定要删除这个房间吗？',
       success: function (res) {
-        console.log('delete success');
-        that.setData({
-          'str': res.data
-        })
-      },
-      fail: function () {
-        that.setData({
-          'str': 'fail'
-        })
+        if (res.confirm) {
+          wx.request({
+            url: app.globalData.serverurl + '/room/delete',
+            data: {
+              room_id: parseInt(that.data.room_id)
+            },
+            dataType: 'json',
+            method: 'POST',
+            success: function (res) {
+              console.log('delete success');
+              that.setData({
+                'str': res.data
+              })
+              wx.showToast({
+                title: '删除成功',
+                icon: 'success',
+                duration:2000
+              })
+              wx.switchTab({
+                url: '../myroom/myroom',
+              })
+            },
+            fail: function () {
+              that.setData({
+                'str': 'fail'
+              })
+            }
+          })
+        } 
+        else if (res.cancel) {
+          //console.log('用户点击取消')
+        }
       }
     })
   },
